1b580d9f3c4217d916cc99da1a92b1385712268b
Powerbounty <playwidhero@gmail.com>
2024-10-21 14:40:50 +1100

    Main js file updated .

--- main.js
+++ main.js
@@ -172,7 +172,7 @@ animate();
 
 // Handle window resize
 window.addEventListener('resize', () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.aspect = window.innerWidth / window.innerHeight; 
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
